22b94feebfb9bb6233823fae81b462ad54f1fa2c
Di Weng <mystery.wd@gmail.com>
2019-03-22 16:00:05 +0800

    convert html encoding

--- cmd/daemon/daemon.go
+++ cmd/daemon/daemon.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html/charset"
 )
 
 // ScrapeConfig defines the configuration for a scrape operation
@@ -36,35 +37,45 @@ type CachedLink struct {
 // UA controls which user agent to use
 const UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"
 
-func get(url string) (*http.Response, error) {
+func get(url string) (*goquery.Document, error) {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Printf("ERROR request %s: %s", url, err)
+		log.Printf("Error while making request %s: %s", url, err)
 		return nil, err
 	}
 
 	req.Header.Set("User-Agent", UA)
-	return client.Do(req)
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error while requesting %s: %s", url, err)
+		return nil, err
+	} else if res.StatusCode != 200 {
+		log.Printf("Error while requesting %s: server responded %d", url, res.StatusCode)
+		return nil, fmt.Errorf("server responded %d", res.StatusCode)
+	}
+
+	contentType := res.Header.Get("Content-Type")
+	reader, err := charset.NewReader(res.Body, contentType)
+	if err != nil {
+		log.Printf("Error while converting encoding %s: %s", url, err)
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		log.Printf("Error while parsing document from %s: %s", url, err)
+		return nil, err
+	}
+
+	return doc, nil
 }
 
 func check(conf *ScrapeConfig, prev []*CachedLink) ([]*CachedLink, []*CachedLink) {
-	res, err := get(conf.MonitorURL)
+	doc, err := get(conf.MonitorURL)
 	if err != nil {
-		log.Printf("ERROR cannot read %s: %s", conf.MonitorURL, err)
-		return prev, nil
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Printf("ERROR cannot read %s: server responded %d", conf.MonitorURL, res.StatusCode)
-		return prev, nil
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Printf("ERROR cannot parse server response: %s", err)
+		log.Printf("Error while checking %s: %s", conf.MonitorURL, err)
 		return prev, nil
 	}
 
@@ -72,14 +83,14 @@ func check(conf *ScrapeConfig, prev []*CachedLink) ([]*CachedLink, []*CachedLink
 
 	results := doc.Find(conf.MonitorLinks)
 	if results.Length() == 0 {
-		log.Printf("WARN no entry returned, skipping")
+		log.Printf("Warning: no entry returned, skipping")
 		return prev, nil
 	}
 
 	results.Each(func(i int, s *goquery.Selection) {
 		url, exists := s.Attr("href")
 		if !exists {
-			log.Printf("ERROR matched a non-link element")
+			log.Printf("Error: matched a non-link element")
 		} else {
 			var title string
 			if conf.Title == "" {
@@ -87,7 +98,7 @@ func check(conf *ScrapeConfig, prev []*CachedLink) ([]*CachedLink, []*CachedLink
 			} else {
 				titleElements := s.Find(conf.Title)
 				if titleElements.Length() == 0 {
-					log.Printf("WARN no title found for %s", url)
+					log.Printf("Warning: no title found for %s", url)
 					title = "Untitled"
 				} else {
 					title = titleElements.Text()
@@ -108,50 +119,40 @@ func check(conf *ScrapeConfig, prev []*CachedLink) ([]*CachedLink, []*CachedLink
 }
 
 func sendPage(link *CachedLink, conf *ScrapeConfig) {
-	res, err := get(link.URL)
+	doc, err := get(link.URL)
 	if err != nil {
-		log.Printf("ERROR cannot read %s: %s", link.URL, err)
+		log.Printf("Error while retrieving %s for sending: %s", conf.MonitorURL, err)
 		return
 	}
-	defer res.Body.Close()
 
-	var content string
-	if res.StatusCode == 200 {
-		doc, err := goquery.NewDocumentFromReader(res.Body)
+	content := ""
+	results := doc.Find(conf.Filter)
+	if results.Length() == 0 {
+		log.Printf("Warning: no element returned from the filter for url %s", conf.MonitorURL)
+		html, err := doc.Html()
 		if err != nil {
-			log.Printf("ERROR cannot parse server response: %s", err)
-			return
-		}
-
-		results := doc.Find(conf.Filter)
-		if results.Length() == 0 {
-			log.Printf("ERROR no content retrieved")
-			content = fmt.Sprintf("<a href=\"%s\">%s</a>", link.URL, link.URL)
+			log.Printf("Error: failed to extract html from the document: %s", err)
 		} else {
-			parts := []string{link.URL}
-			results.Each(func(i int, s *goquery.Selection) {
-				html, err := s.Html()
-				if err != nil {
-					log.Printf("ERROR failed to extract html: %s", err)
-				} else {
-					parts = append(parts, html)
-				}
-			})
-			content = strings.Join(parts, "<hr>")
+			content = html
 		}
-	} else if res.StatusCode == 302 {
-		log.Printf("INFO link is a redirect")
-		content = fmt.Sprintf("<a href=\"%s\">%s</a>", link.URL, link.URL)
 	} else {
-		log.Printf("ERROR cannot read %s: server responded %d", link.URL, res.StatusCode)
-		return
+		partials := []string{}
+		results.Each(func(i int, s *goquery.Selection) {
+			html, err := s.Html()
+			if err != nil {
+				log.Printf("Error: failed to extract html from the document: %s", err)
+			} else {
+				partials = append(partials, html)
+			}
+		})
+		content = strings.Join(partials, "<hr>")
 	}
 
-	msg := fmt.Sprintf("Subject: %s\nTo: %s\nMIME-version: 1.0;\nContent-Type: text/html;\n\n%s",
-		link.Title, conf.Email, content)
+	msg := fmt.Sprintf("Subject: %s\nTo: %s\nMIME-version: 1.0;\nContent-Type: text/html; charset=utf-8\n\n%s<hr>%s",
+		link.Title, conf.Email, link.URL, content)
 	err = smtp.SendMail(conf.SMTPServer, nil, "go_web_page_to_email", []string{conf.Email}, []byte(msg))
 	if err != nil {
-		log.Printf("ERROR send mail failed: %s", err)
+		log.Printf("Error: failed to send email: %s", err)
 	}
 }
 
@@ -177,11 +178,10 @@ func main() {
 	)
 
 	for {
-
-		log.Printf("checking %s...", conf.Tag)
+		// log.Printf("checking %s...", conf.Tag)
 		base, err := url.Parse(conf.MonitorURL)
 		if err != nil {
-			log.Printf("ERROR URL expected for \"%s\": %s", conf.MonitorURL, err)
+			log.Printf("Error URL expected for \"%s\": %s", conf.MonitorURL, err)
 			continue
 		}
 		cache, news = check(&conf, cache)
@@ -197,7 +197,7 @@ func main() {
 				for _, n := range news {
 					u, err := url.Parse(n.URL)
 					if err != nil {
-						log.Printf("ERROR URL expected for \"%s\": %s", n, err)
+						log.Printf("Error: url expected for \"%s\": %s", n, err)
 					} else {
 						link := CachedLink{
 							Title: conf.Tag + " | " + n.Title,
@@ -207,10 +207,8 @@ func main() {
 					}
 				}
 			}
-		} else {
-			log.Printf("no new entry found.")
 		}
-		log.Printf("waiting for next check...")
+		// log.Printf("waiting for next check...")
 		time.Sleep(time.Duration(conf.Delay) * time.Second)
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/w1ndy/go-webpage-to-email
 
-require github.com/PuerkitoBio/goquery v1.5.0
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a
+	golang.org/x/text v0.3.0 // indirect
+)
